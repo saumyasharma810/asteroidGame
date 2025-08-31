c9e95d631f3e0855c467d92a7bb3c8d5215e8c97
saumyasharma810 <saumya.sharma810@gmail.com>
2025-08-31 22:51:03 +0530

    Add scoring system

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from shot import Shot
 
 def main():
     pygame.init()
+    pygame.font.init() 
+    font = pygame.font.SysFont('Arial', 32)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -31,26 +33,50 @@ def main():
     # 1. Check for player inputs
     # 2. Update the game world
     # 3. Draw the game to the screen
+
+    game_over = False
+    player_score = 0
     
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill(color=(0,0,0))
-        for obj in drawable:
-            obj.draw(screen)
-        updatable.update(dt)
-        for obj in asteroids:
-            if player.collision(obj):
-                print("Game over!")
-                exit(0)
-        for asteroid in asteroids:
-            for shot in all_shots:
-                if asteroid.collision(shot):
-                    asteroid.split()
-                    shot.kill()
-        pygame.display.flip()
-        dt = clock.tick(float(60))/1000
+        if not game_over:
+            score_text = font.render(f"Score: {player.score}", True, (255, 255, 255))
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            screen.fill(color=(0,0,0))
+            for obj in drawable:
+                obj.draw(screen)
+            screen.blit(score_text, (10, 10))
+            updatable.update(dt)
+            for obj in asteroids:
+                if player.collision(obj):
+                    print(f"Game over! Your score is {player.score}")
+                    game_over = True
+                    player_score = player.score
+                    break
+                    # exit(0)
+            if game_over:
+                continue
+            for asteroid in asteroids:
+                for shot in all_shots:
+                    if asteroid.collision(shot):
+                        player.score += asteroid.split()
+                        shot.kill()
+            pygame.display.flip()
+            dt = clock.tick(float(60))/1000
+
+        else:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            screen.fill((0,0,0))
+            score_text = font.render(f"Your Score: {player_score}", True, (255, 255, 255))
+            screen.blit(score_text, ((SCREEN_WIDTH - score_text.get_width()) // 2, (SCREEN_HEIGHT - score_text.get_height()) // 2))
+            pygame.display.flip()
+            clock.tick(30)
+
+    
 
 
 
